d4314e2736101878b5ace61b12fbda9e588d052d
Eric Hopkins <scarecrow3322@gmail.com>
2014-05-21 18:08:04 -0700

    Added Pilot class, and implemented it in Mech
    The pilot class is no longer a todo, and has been implemented. And the
    Mech class now correctly has a member class of Pilot.

--- Battletech_RAT_v2/vb files/Mech.vb
+++ Battletech_RAT_v2/vb files/Mech.vb
@@ -1,10 +1,38 @@
 ﻿Public Class Mech
 
-    'TODO: implement a pilot class so that each mech can have a member pilot class
+    Protected pilot As Pilot
     Property Name As String
 
     Public Sub New(ByVal value As String)
 
         Name = value
     End Sub
+
+    Public Sub addPilot(ByRef pilotPassed As Pilot)
+
+        Me.pilot.Name = pilotPassed.Name
+        Me.pilot.GunnerySkill = pilotPassed.GunnerySkill
+        Me.pilot.PilotSkill = pilotPassed.PilotSkill
+    End Sub
+
+    Public Sub RemovePilot()
+
+        ' There's no way to set a value to null in vb, so
+        ' I just made the pilot skills really terrible
+        Me.pilot.Name = ""
+        Me.pilot.GunnerySkill = 8
+        Me.pilot.PilotSkill = 9
+    End Sub
+
+    Public Overrides Function ToString() As String
+
+        ' ensure that pilot contains something
+        If (pilot.Name = "") Then
+
+            Return Name
+        Else
+
+            Return Name & ": " & pilot.Name & " (" & pilot.GunnerySkill & "/" & pilot.PilotSkill & ")"
+        End If
+    End Function
 End Class
---
+++ Battletech_RAT_v2/vb files/Pilot.vb
@@ -0,0 +1,13 @@
+﻿Public Class Pilot
+
+    Property Name As String
+    Property PilotSkill As Integer
+    Property GunnerySkill As Integer
+
+    Public Sub New(ByVal name As String, ByVal piloting As Integer, ByVal gunnery As Integer)
+
+        Me.Name = name
+        Me.PilotSkill = piloting
+        Me.GunnerySkill = gunnery
+    End Sub
+End Class
